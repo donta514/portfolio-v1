d16c4caf8f904d2c809855a7e8b2e29c6eabd8fc
Donta Smith <dnovsmith.ds20@gmail.com>
2024-09-23 22:27:29 +0000

    implemented light / dark mode toggle

---
+++ astro/src/styles/theme.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  htmlBackground: "#f0f4f8",
+  textFont: "#1a202c",
+  buttonBackground: "rgb(136, 58, 234)",
+  buttonHoverBackground: "rgb(49, 10, 101)",
+  cardBackground: "#ffffff",
+  borderColor: "rgb(226, 232, 240)",
+};
+
+export const darkTheme = {
+  htmlBackground: "#0d1117",
+  textFont: "#ffffff",
+  buttonBackground: "rgb(136, 58, 234)",
+  buttonHoverBackground: "rgb(49, 10, 101)",
+  cardBackground: "#13151a",
+  borderColor: "rgb(48, 54, 61)",
+};
